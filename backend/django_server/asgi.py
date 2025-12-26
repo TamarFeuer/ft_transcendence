e52cae38d3dae0e75f63d817f6ff8c2845df14a7
Tamar Feuer <tamar2603@gmail.com>
2025-12-26 22:25:44 +0100

    feat(chat): add real-time chat system
    - Add `chat` folder in backend with `ChatConsumer` for WebSocket chat.
    - Add `chat.js` in frontend `src` with `chatSocket` to connect and send messages.
    - Pressing chat button now broadcasts a message to all connected clients.

--- backend/django_server/asgi.py
+++ backend/django_server/asgi.py
@@ -2,12 +2,14 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from game.routing import websocket_urlpatterns
+from game.routing import websocket_urlpatterns as game_ws
+from chat.routing import websocket_urlpatterns as chat_ws
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_server.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(game_ws + chat_ws)
     ),})
--- backend/django_server/settings.py
+++ backend/django_server/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'game',
+    'chat',
 ]
 
 MIDDLEWARE = [
