3c43b8a3dfcd6a2b82299ad82382a88ffde42422
Rupsenjager <146854614+Rupsenjager@users.noreply.github.com>
2026-04-10 11:19:46 +0200

    base of the homepage

---
+++ frontend/src/home/home.js
@@ -0,0 +1,12 @@
+import { navigate } from "../routes/route_helpers.js";
+
+export function initHome() {
+    const username = localStorage.getItem('username');
+
+    document.getElementById('navAvatar').textContent = username.charAt(0).toUpperCase();
+    document.getElementById('navUsername').textContent = username;
+    document.getElementById('profileNavBtn')?.addEventListener('click', () => navigate('/profile'));
+    document.getElementById('aiBtn')?.addEventListener('click', () => navigate('/ai'));
+    document.getElementById('onlineBtn')?.addEventListener('click', () => navigate('/online'));
+    document.getElementById('localBtn')?.addEventListener('click', () => navigate('/local'));
+}
