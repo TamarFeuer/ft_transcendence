72ede9799233d49ba9d5b08412c34bcb1290b2f0
Stan Nijhuis <snijhuis@f0r1s2.codam.nl>
2026-04-16 13:47:33 +0200

    added button only visually

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -2,6 +2,7 @@ import { navigate } from "../routes/route_helpers.js";
 
 export function initHome() {
     const username = localStorage.getItem('username');
+    const mode = localStorage.getItem('gameMode') || 'pong';
 
     document.getElementById('navAvatar').textContent = username.charAt(0).toUpperCase();
     document.getElementById('navUsername').textContent = username;
