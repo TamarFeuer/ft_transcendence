f991d1d6f404ef43d1785935e0d5f8b1ea1963d1
Stan Nijhuis <snijhuis@f0r1s2.codam.nl>
2026-04-16 14:51:42 +0200

    switch button made

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -2,11 +2,21 @@ import { navigate } from "../routes/route_helpers.js";
 
 export function initHome() {
     const username = localStorage.getItem('username');
-    const mode = localStorage.getItem('gameMode') || 'pong';
-
+    
     document.getElementById('navAvatar').textContent = username.charAt(0).toUpperCase();
     document.getElementById('navUsername').textContent = username;
+    
+    
+    document.getElementById("toggle-game-mode").addEventListener('click', () => {
+        const current = localStorage.getItem('gameMode') || 'pong';
+        let newMode;
+        if (current === 'pong')
+            newMode = 'chess';
+        else
+            newMode = 'pong';
 
+        const toggleBtn = document.getElementById('toggle=btn');
+    })
 
     //change depending on which mode you are. chess or pong.
     document.getElementById('profileNavBtn')?.addEventListener('click', () => navigate('/profile'));
