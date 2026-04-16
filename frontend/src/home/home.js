a010a863e4ce8486401cf8dc2dbafad5efdc5ad4
Stan Nijhuis <snijhuis@f0r1s2.codam.nl>
2026-04-16 15:30:45 +0200

    navigate not working

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -9,6 +9,9 @@ export function initHome() {
     
     document.getElementById("toggle-game-mode").addEventListener('click', () => {
         const current = localStorage.getItem('gameMode') || 'pong';
+        const pongText = document.getElementById("pong");
+        const chessText = document.getElementById("chess")
+
         let newMode;
 
         if (current === 'pong')
@@ -22,10 +25,14 @@ export function initHome() {
         if(newMode === 'chess'){
             toggleBtn.style.left = '';
             toggleBtn.style.right = '0.25rem';
+            pongText.className = "text-zinc-500 font-semibold text-xl";
+            chessText.className = "text-white font-semibold text-xl";
         }
         else{
             toggleBtn.style.left = '0.25rem';
             toggleBtn.style.right = '';
+            pongText.className = "text-white font-semibold text-xl";
+            chessText.className = "text-zinc-500 font-semibold text-xl";
         }
     })
 
@@ -33,28 +40,26 @@ export function initHome() {
 
     
     document.getElementById('aiBtn')?.addEventListener('click', () => {
-        const mode = localStorage('gameMode');
+        const mode = localStorage.getItem('gameMode');
         if(mode === 'pong')
             navigate('ai');
     });
 
     document.getElementById('onlineBtn')?.addEventListener('click', () => {
-        const mode = localStorage('gameMode');
+        const mode = localStorage.getItem('gameMode');
         
         if (mode === 'pong')
             navigate('/online');
         else
-            navigate('online-chess');
+            navigate('/online-chess');
     });
 
-
-    nagivate('chess')
     document.getElementById('localBtn')?.addEventListener('click', () => {
-        const mode = localStorage('gameMode');
+        const mode = localStorage.getItem('gameMode');
         
         if(mode === 'pong')
             navigate('/local')
         else
-            navigate('chess');
+            navigate('/chess');
     });
 }
