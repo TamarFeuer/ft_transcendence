e71ac2c2ea12267057fa7ffe009ece7a043841ed
Nikos <sarmadakis.nikos@gmail.com>
2026-04-21 14:36:43 +0200

    backend matchmaking pong is done

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('game/create', views.create_game, name='create_game'),
     path('game/record-local-match/', views.record_local_match, name='record_local_match'),
     path('game/<str:game_id>', views.get_game, name='get_game'),
+    path('game/join', views.join_pong, name='join_pong'),
     path('games', views.list_games, name='list_games'),
     path('leaderboard', views.get_leaderboard, name='get_leaderboard'),
     path('match-history', views.match_history, name='match_history'),
