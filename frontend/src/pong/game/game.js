2b35b83f1334ea5319a2857ff3ee9f6d508c85bb
Rupsenjager <146854614+Rupsenjager@users.noreply.github.com>
2026-04-24 11:13:24 +0200

    removed comment

--- frontend/src/pong/game/game.js
+++ frontend/src/pong/game/game.js
@@ -145,16 +145,7 @@ export function joinOnlineGame(gameId, IsTournament) {
   let keyUpHandler = null;
   let resizeHandler = null;
   let gameEnded = false;
-
-  // const proto = location.protocol === "https:" ? "wss:" : "ws:";
-  // Cookies are automatically sent with WebSocket connections
-  // Connect to backend on port 3000 (not vite dev server on 5173)
-  // const wsHost = import.meta.env.DEV ? 'localhost:3000' : location.host;
-
-  // console.log("DEV import:", import.meta.env.DEV);
-  // console.log("WS Host:", wsHost);
-  // console.log("location.host:", location.host);
-
+  
   const proto = location.protocol === "https:" ? "wss:" : "ws:";
   ws = new WebSocket(`${proto}//${location.host}/ws/${gameId}`);
 
